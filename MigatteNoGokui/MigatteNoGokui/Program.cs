819f13a3beada3093b77b044419501e8deac7fb5
Oscar Eduardo Blandon Alvarez <39421508+Oskr814@users.noreply.github.com>
2019-04-22 09:38:00 -0600

    sepala

--- MigatteNoGokui/MigatteNoGokui/Program.cs
+++ MigatteNoGokui/MigatteNoGokui/Program.cs
@@ -3,17 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace MigatteNoGokui
 {
     static class Program
     {
+        
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            string carpeta = @"C:\logs_usuario";
+            try
+            {
+                if (!Directory.Exists(carpeta));
+                {
+                    Directory.CreateDirectory(carpeta);
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MigatteNoGokui());
---
+++ MigatteNoGokui/MigatteNoGokui/bin/x64/Debug/logs_usuario/ejecucion.bat
@@ -0,0 +1,1 @@
+tasklist /V /FI "imagename eq chrome.exe" /FO csv > logs_usuario\logs.txt
